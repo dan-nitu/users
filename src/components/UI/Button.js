a499d22d65a0de53278ec55f74ea217919823a65
Voodoo30fps <voodoo30fps@gmail.com>
2023-07-04 15:12:47 +0300

    card and button components

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,7 +1,15 @@
 import classes from './Button.module.css';
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = (props) => {
+  return (
+    <button
+      type={props.type || 'button'}
+      className={classes.button}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
 };
 
 export default Button;
