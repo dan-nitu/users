0ad6104fd01bf230b37b4db3b78ee7851dbb07d5
Voodoo30fps <voodoo30fps@gmail.com>
2023-07-04 15:13:05 +0300

    add user jsx

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,7 +1,29 @@
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+
 import classes from './AddUser.module.css';
 
 const AddUser = () => {
-  return <div>AddUser</div>;
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <div>
+          <label htmlFor='username'>Username</label>
+          <input id='username' type='text' />
+        </div>
+        <div>
+          <label htmlFor='age'>Age</label>
+          <input id='age' type='number' />
+        </div>
+
+        <Button type={'submit'}>Add User</Button>
+      </form>
+    </Card>
+  );
 };
 
 export default AddUser;
